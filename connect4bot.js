4d331037e710b9f7da12d2db38e2f33e5047a5d3
Wallace Luk <wallace@Wallaces-MacBook-Air.local>
2016-12-15 15:01:22 -0800

    Add noop game loop

--- connect4bot.js
+++ connect4bot.js
@@ -6,23 +6,69 @@ if(!process.env.token) {
 const Botkit = require('botkit');
 const controller = Botkit.slackbot({
   debug: false
-  //include "log: false" to disable logging
-  //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
 });
 
 // connect the bot to a stream of messages
 controller.spawn({
   token: process.env.token,
-}).startRTM()
+}).startRTM();
 
+const makeBoard = () => {
+  const res = [];
+  for(var i = 0; i < 7; i++) {
+    const row = [];
+    for(var j = 0; j < 7; j++) {
+      row.push(0);
+    }
+    res.push(row);
+  }
+  return res;
+}
+
+//Asks the opponent for a move
+const getResponse = (bot, id, cb) => {
+  bot.startPrivateConversation({ user: id }, (err, convo) => {
+
+  }); 
+};
+
+//Checks the board to see if there is a connect 4 or a tie
+//Returns -1 if game is not over
+//Returns 0 if game is a tie
+//Returns 1 if player one wins
+//Returns 2 if player two wins
+const evaluate = (board) => {
+  return 0;
+};
 
 const startGame = (bot, message, one, two) => {
-  console.log(one, two);
-  bot.reply(message, 'Starting match!');
-  bot.startPrivateConversation({ user: two }, (err, convo) => {
+  let board = makeBoard();
+  let matchEnd = -1;
+  let turn = true;
+
+  bot.startConversation(message, (err, convo) => {
     convo.say('Starting match!');
+
+    //Game loop
+    while(matchEnd === -1) {
+      if(turn){
+        convo.ask('Make your move! [1-7]', (response, convo) => { 
+          //update board state
+        });
+        matchEnd = evaluate(board);
+        turn = !turn;
+      } else {
+        getResponse(bot, two, (response, convo) => {
+          //update board state
+        });
+        matchEnd = evaluate(board);
+        turn = !turn;
+      }
+    }
+
   });
 };
+
 
 controller.hears('play',['direct_message'], (bot, message) => {
   const data = message.text.match(/<@([A-Z0-9]{9})>/);
