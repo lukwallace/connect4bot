d7dd9e92e6ee31a45c11975b5b0df28e894aa668
Wallace Luk <wallace@Wallaces-MacBook-Air.local>
2016-12-16 17:27:49 -0800

    Add render & eval

--- connect4bot.js
+++ connect4bot.js
@@ -18,50 +18,123 @@ const makeBoard = () => {
   for(var i = 0; i < 7; i++) {
     const row = [];
     for(var j = 0; j < 7; j++) {
-      row.push(0);
+      row.push('E');
     }
     res.push(row);
   }
   return res;
 }
 
+const render = (board) => {
+  let res = '';
+  for(let i = 0; i < board.length; i++) {
+    for(let j = 0; j < board.length; j++) {
+      if(board[i][j] === 'E') {
+        res = res + ':white_medium_square:';
+      }
+      if(board[i][j] === 'R'){
+        res = res + ':red_circle:';
+      }
+      if(board[i][j] === 'B'){
+        res = res + ':black_circle:';
+      }
+    }
+    res = res + '\n';
+  }
+  return res;
+};
+
 //Asks player for a move. If the message object is passed
 //to it, that means the it's from player one
-const getResponse = (bot, id, cb, message) => {
+const getResponse = (board, bot, id, cb, message) => {
+  const theBoard = render(board);
   if(message) {
     bot.startConversation(message, (err, convo) => {
-      convo.ask('Make your move! [1-7]', (response, convo) => {
-        cb(response, convo);
-      });
+      convo.ask(theBoard + '\nMake your move! [1-7]', cb);
     });
   } else {
     bot.startPrivateConversation({ user: id }, (err, convo) => {
-      convo.ask('Make your move! [1-7]', (response, convo) => {
-        cb(response, convo);
-      });
+      convo.ask(theBoard + '\nMake your move! [1-7]', cb);
     });
   }
 };
 
-//Updates the board
+//Updates the board with user input
 //Returns 1 on success
 //Returns 0 if the column is full
 const update = (board, num, turn) => {
-  for(let i = 0; i < board.length; i++) {
-    if(board[i][num] === 0) {
-      board[i][num] = turn ? 1 : -1;
+  for(let i = board.length-1; i >= 0; i--) {
+    if(board[i][num] === 'E') {
+      board[i][num] = turn ? 'R' : 'B';
       return 1;
     }
   };
   return 0;
 };
 
+
 //Checks the board to see if there is a connect 4 or a tie
 //Returns -1 if game is not over
-//Returns 0 if game is a tie
+//Returns 0 if game is a tie (NOOP)
 //Returns 1 if player one wins
 //Returns 2 if player two wins
 const evaluate = (board) => {
+  console.log(board);
+  //check rows
+  console.log('Checking rows');
+  for(let i = 0; i < board.length; i++) {
+    const row = board[i].join('');
+    if(row.indexOf('RRRR') !== -1) {
+      return 1;
+    }
+    if(row.indexOf('BBBB') !== -1) {
+      return 2;
+    }
+  }
+  //check columns
+  console.log('Checking columns');
+  for(let i = 0; i < board.length; i++) {
+    const arr = [];
+    for(let j = 0; j < board.length; j++) {
+      arr.push(board[j][i]);
+    }
+    const col = arr.join('');
+    if(col.indexOf('RRRR') !== -1) {
+      return 1;
+    }
+    if(col.indexOf('BBBB') !== -1) {
+      return 2;
+    }
+  }
+
+  //check diagonals
+  console.log('Checking diagonals');
+  const leftward = {};
+  const rightward = {};
+  for(let i = 0; i < board.length; i++) {
+    for(let j = 0; j < board.length; j ++) {
+      leftward[i-j] = leftward[i-j] ? leftward[i-j] + board[i][j] : board[i][j];
+      rightward[i+j] = rightward[i+j] ? rightward[i+j] + board[i][j]:  board[i][j];
+    }
+  }
+
+  const leftKeys = Object.keys(leftward);
+  const rightKeys = Object.keys(rightward);
+  for(let i = 0; i < leftKeys.length; i++) {
+    if(leftward[leftKeys[i]].indexOf('RRRR') !== -1) {
+      return 1;
+    }
+    if(leftward[leftKeys[i]].indexOf('BBBB') !== -1) {
+      return 2;
+    }
+    if(rightward[rightKeys[i]].indexOf('RRRR') !== -1) {
+      return 1;
+    }
+    if(rightward[rightKeys[i]].indexOf('BBBB') !== -1) {
+      return 2;
+    }
+  }
+
   return -1;
 };
 
@@ -70,29 +143,35 @@ const startGame = (bot, message, one, two) => {
   let matchEnd = -1;
   let turn = true;
 
+  console.log('Starting game!');
   //Game loop
   const takeTurn = () => {
-    getResponse(bot, two, (response, convo) => {
+    getResponse(board, bot, two, (response, convo) => {
       //update board state
       const res = response.text;
       if(isNaN(res)){
         convo.repeat();
         convo.next();
       } else if (+res > 7 || +res < 1){
-        console.log('Bad input!');
         convo.repeat();
         convo.next();
       } else {
         const didUpdate = update(board, +res - 1, turn);
-        console.log(board, didUpdate);
         if(!didUpdate) {
           convo.repeat();
           convo.next();
         } else {
+          convo.say(render(board));
+          convo.say('Thanks!');
           matchEnd = evaluate(board);
-          turn = !turn;
-          convo.next();
-          takeTurn();
+          if(matchEnd !== -1) {
+            convo.say('GG');
+            convo.next();
+          } else {
+            turn = !turn;
+            convo.next();
+            takeTurn();
+          }
         }
       }
     }, turn ? message : null);      
